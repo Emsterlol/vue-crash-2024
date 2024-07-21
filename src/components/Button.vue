7ad6a70dfd024daf5d378920f507c6f26cb7e59c
Emily Horne <128605374+Emsterlol@users.noreply.github.com>
2024-07-21 17:40:20 -0400

    Add files via upload

---
+++ src/components/Button.vue
@@ -0,0 +1,47 @@
+<template>
+    <button @click="onClick" :style=" { background: color }" class="btn">{{ text }} </button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Button',
+    props: {
+      text: {
+        type: String,
+        required: true,
+      },
+      color: {
+        type: String,
+        required: true,
+      },
+    },
+    methods: {
+      onClick() {
+        this.$emit('btn-click');
+      }
+    }
+  }
+  </script>
+
+<style scoped>
+.btn {
+  color: white;
+  font-size: 15px;
+  padding: 10px 20px;
+  margin: 5px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  display: inline-block;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+</style>
